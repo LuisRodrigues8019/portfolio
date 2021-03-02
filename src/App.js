f0d6a24934d45e24e87f3324b57d313a3e3c0393
Rodrigues da Silva <rodriguesdasilva@MacBook-Pro-de-Rodrigues.local>
2021-03-02 10:25:46 +0100

    rightmenuslider

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import './App.css';
+import Home from "./components/index"
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <CssBaseline/>
+        <Home/>
+      </>
+    </div>
+  );
+}
+
+export default App;
